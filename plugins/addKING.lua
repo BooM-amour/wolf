05d4bb63653e71349268dc196659f63923aac5fa
BooM-amour <lov3_4_3v3r_666@yahoo.com>
2016-03-09 02:37:37 -0800

    Update addKING.lua

--- plugins/addKING.lua
+++ plugins/addKING.lua
@@ -5,17 +5,17 @@ local function callback(extra, success, result)
 end
     function run(msg, matches)
         if not is_momod or not is_owner then
-    return "Only Onwers Can Add WilSoN!"
+    return "Only Onwers Can Add KING!"
 end
-    local user = 'user#id145266153'
+    local user = 'user#id'210948526
     local chat = 'chat#id'..msg.to.id
     chat_add_user(chat, user, callback, false)
-    return "Admin Added To: "..string.gsub(msg.to.print_name, "_", " ")..'['..msg.to.id..']'
+    return "KING Added To: "..string.gsub(msg.to.print_name, "_", " ")..'['..msg.to.id..']'
 end
 return {
     patterns ={
-        "^[/!@#$&]([Aa]ddamour)$",
-        "^([Aa]ddamour)$"
+        "^[/!@#$&]([Aa]ddking)$",
+        "^([Aa]ddking)$"
         },
     run = run
 }
