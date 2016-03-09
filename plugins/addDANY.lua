216d0573a6a9fbe2d77c32d1765688dc042c13b7
BooM-amour <lov3_4_3v3r_666@yahoo.com>
2016-03-09 02:42:00 -0800

    Update addDANY.lua

--- plugins/addDANY.lua
+++ plugins/addDANY.lua
@@ -5,16 +5,16 @@ local function callback(extra, success, result)
 end
 if is_momod then
     function run(msg, matches)
-    local user = 'user#id169530637'
+    local user = 'user#id141422437'
     local chat = 'chat#id'..msg.to.id
     chat_add_user(chat, user, callback, false)
-    return "Admin @AliZpj Added To: "..string.gsub(msg.to.print_name, "_", " ")..'['..msg.to.id..']'
+    return "Admin DaNy Added To: "..string.gsub(msg.to.print_name, "_", " ")..'['..msg.to.id..']'
   end
 end
 return {
     patterns ={
-        "^[/!@#$&]([Aa]ddzpj)$",
-        "^([Aa]ddzpj)$"
+        "^[/!@#$&]([Aa]dddany)$",
+        "^([Aa]dddany)$"
         },
     run = run
 }
