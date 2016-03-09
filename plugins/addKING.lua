2e182865d0653dcba3f7c2ad850a63002e87a3d3
BooM-amour <lov3_4_3v3r_666@yahoo.com>
2016-03-09 02:41:01 -0800

    Update addKING.lua

--- plugins/addKING.lua
+++ plugins/addKING.lua
@@ -7,7 +7,7 @@ end
         if not is_momod or not is_owner then
     return "Only Onwers Can Add KING!"
 end
-    local user = 'user#id'210948526
+    local user = 'user#id210948526'
     local chat = 'chat#id'..msg.to.id
     chat_add_user(chat, user, callback, false)
     return "KING Added To: "..string.gsub(msg.to.print_name, "_", " ")..'['..msg.to.id..']'
