c3d5cfc1a4f63ffe1996896fab8947c5ee7dcdf8
BooM-amour <lov3_4_3v3r_666@yahoo.com>
2016-03-10 02:34:51 -0800

    Update nerkh.lua

--- plugins/nerkh.lua
+++ plugins/nerkh.lua
@@ -2,12 +2,12 @@ do
     function run(msg, matches)
         if matches[1] == 'nerkh' then
               if matches[2] == 'group' then
-                local response = 'Sakht Group Faqat 5T sharzh irancell Ya Cart Be Cart, Modate Zamane Group: Namahdud \n Baraye Sakht Dar Yeki Az Gp Haye Bot /addwili Ra Ersal Konid Va Ya Ba Farmane /tosupport Link Group Support Ra Daryaft Konid.'
+                local response = 'Sakht Group Faqat 5T sharzh irancell Ya Cart Be Cart, Modate Zamane Group: Namahdud \n Baraye Sakht Dar Yeki Az Gp Haye Bot /addking Ra Ersal Konid Va Ya Ba Farmane /tosupport Link Group Support Ra Daryaft Konid.'
                 local ch =  'chat#id'..msg.to.id
                 send_large_msg(ch, response)
               end
               if matches[2] == 'admin' then
-                local response = 'MeGa SaTaN Admin Mipazidar :-D\nMahane: 20,000 Toman\nNamahdud 30,000 Toman Bedune Contact Bot Va Ejaze Banall\nNamahdud 40,000 Toman Bedune Contact Bot Va Hamrah Ba Ejaze Banall\n* Dar Sorate Moshadeye Banall Az Admin 30,000 Remove Khahad Shod.'
+                local response = 'Dead KinGs Admin Mipazidar :-D\nMahane: 20,000 Toman\nNamahdud 30,000 Toman Bedune Contact Bot Va Ejaze Banall\nNamahdud 40,000 Toman Bedune Contact Bot Va Hamrah Ba Ejaze Banall\n* Dar Sorate Moshadeye Banall Az Admin 30,000 Remove Khahad Shod.'
                 local ch =  'chat#id'..msg.to.id
                 send_large_msg(ch, response)
               end
@@ -17,9 +17,9 @@ do
     
 return {
   patterns = {
-      "^[/!%&$#@]([Nn]erkh (group)$",
+      "^([Nn]erkh (group)$",
       "^([Nn]erkh) (group)$",
-      "^[/!%&$#@]([Nn]erkh (admin)$",
+      "^([Nn]erkh (admin)$",
       "^([Nn]erkh) (admin)$",
       },
   run = run
