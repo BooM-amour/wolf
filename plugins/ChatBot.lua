5d3ab5d2743f56f513d3edac81be162f34e4ff54
BooM-amour <lov3_4_3v3r_666@yahoo.com>
2016-03-10 02:17:22 -0800

    Update ChatBot.lua

--- plugins/ChatBot.lua
+++ plugins/ChatBot.lua
@@ -1,31 +1,31 @@
 --shared by @blackhatchannel
 local function run(msg)
 if msg.text == "hi" then
-	return "Hello bb"
+	return "Hello "
 end
 if msg.text == "Hi" then
-	return "Hello honey"
+	return "Hello "
 end
 if msg.text == "Hello" then
-	return "Hi bb"
+	return "Hi hony"
 end
 if msg.text == "hello" then
-	return "Hi honey"
+	return "Hi "
 end
 if msg.text == "Salam" then
-	return "Salam aleykom"
+	return "slm"
 end
 if msg.text == "salam" then
-	return "va aleykol asalam"
+	return " asalam"
 end
-if msg.text == "blackhat" then
-	return "my love blackhat"
+if msg.text == "dany" then
+	return "my owner is dany"
 end
-if msg.text == "Blackhat" then
-	return "my father blackhat"
+if msg.text == "Dany" then
+	return "dany baba"
 end
-if msg.text == "BLACKHAT" then
-	return "Kheily Radifeh"
+if msg.text == "KING" then
+	return "what you say?"
 end
 if msg.text == "Security" then
 	return "Yes?"
@@ -37,16 +37,16 @@ if msg.text == "bot" then
 	return "hum?"
 end
 if msg.text == "Bot" then
-	return "Huuuum?"
+	return "WTF?"
 end
 if msg.text == "?" then
-	return "Hum??"
+	return "tell me??"
 end
 if msg.text == "Bye" then
-	return "Babay"
+	return "GooD LUCK"
 end
 if msg.text == "bye" then
-	return "boro bay"
+	return "GO get sleep"
 end
 end
 
@@ -56,8 +56,8 @@ return {
 	patterns = {
 		"^[Hh]i$",
 		"^[Hh]ello$",
-		"^[Bb]lackhat$",
-		"^BLACKHAT$",
+		"^[Dd]any$",
+		"^KING$",
 		"^[Bb]ot$",
 		"^[Ss]ecurity$",
 		"^[Bb]ye$",
